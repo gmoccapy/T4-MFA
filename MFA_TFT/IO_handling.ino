41f96b62a09a76b668fff7a3cb1f896f04a4a5ed
Norbert Schechner <nieson@web.de>
2025-10-06 15:06:04 +0200

    Ne IO Handling after hardware correction
    Signed-off-by: Norbert Schechner <nieson@web.de>

--- MFA_TFT/IO_handling.ino
+++ MFA_TFT/IO_handling.ino
@@ -22,50 +22,31 @@ void check_IO(int PIN, bool mcp_state){
     case PIN_MEMORY:
       Serial.print("PIN_MEMORY IO has changed to ");
       Serial.println(mcp_state);
+
       break;
 
-    case PIN_BRAKEPADS:
-      Serial.print("PIN_BRAKEPADS IO has changed to ");
+    case PIN_RESET:
+      Serial.print("PIN_RESET IO has changed to ");
       Serial.println(mcp_state);
-      if(mcp_state == true){
-        tft.drawXBitmap(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], sym_brakepads, 50, 50, TFT_ORANGE);
-      }
-      else{
-        tft.fillRect(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], 50, 50, BACK_COLOR);
-      }
+
       break;
 
-    case PIN_BRAKESYSTEM:
-      Serial.print("PIN_BRAKESYSTEM IO has changed to ");
+    case PIN_TRUNK:
+      Serial.print("PIN_TRUNK IO has changed to ");
       Serial.println(mcp_state);
-      if(mcp_state == true){
-        tft.drawXBitmap(Icon_Pos_BrakeSystem[0], Icon_Pos_BrakeSystem[1], sym_brakesystem, 50, 50, TFT_RED);
-      }
-      else{
-        tft.fillRect(Icon_Pos_BrakeSystem[0], Icon_Pos_BrakeSystem[1], 50, 50, BACK_COLOR);
-      }
+
       break;
 
-    case PIN_WASHER_FLUID:
-      Serial.print("PIN_WASHER_FLUID IO has changed to ");
+    case PIN_R_DOOR:
+      Serial.print("PIN_R_DOOR IO has changed to");
       Serial.println(mcp_state);
-      if(mcp_state == true){
-        tft.drawXBitmap(Icon_Pos_WasherFluid[0], Icon_Pos_WasherFluid[1], sym_washer_fluid, 50, 50, TFT_ORANGE);
-      }
-      else{
-        tft.fillRect(Icon_Pos_WasherFluid[0], Icon_Pos_WasherFluid[1], 50, 50, BACK_COLOR);
-      }
+
       break;
 
-    case PIN_OIL_LEVEL:
-      Serial.print("PIN_OIL_LEVEL IO has changed to ");
+    case PIN_S_DOOR:
+      Serial.print("PIN_S_DOOR IO has changed to ");
       Serial.println(mcp_state);
-      if(mcp_state == true){
-        tft.drawXBitmap(Icon_Pos_Oil[0], Icon_Pos_Oil[1], sym_oil, 50, 50, TFT_ORANGE);
-      }
-      else{
-        tft.fillRect(Icon_Pos_Oil[0], Icon_Pos_Oil[1], 50, 50, BACK_COLOR);
-      }
+
       break;
 
     case PIN_OIL_PRESURE:
@@ -80,32 +61,72 @@ void check_IO(int PIN, bool mcp_state){
       break;
 
     case PIN_COOLANT:
-      Serial.println("PIN_COOLANT IO has changed to ");
-      Serial.print(mcp_state);
+      Serial.print("PIN_COOLANT IO has changed to ");
+      Serial.println(mcp_state);
       if(mcp_state == true){
         tft.drawXBitmap(Icon_Pos_Coolant[0], Icon_Pos_Coolant[1], sym_coolant, 50, 50, TFT_RED);
       }
       else{
         tft.fillRect(Icon_Pos_Coolant[0], Icon_Pos_Coolant[1], 50, 50, BACK_COLOR);
       }
-
-    case PIN_R_DOOR:
-      Serial.print("PIN_R_DOOR IO has changed to");
-      Serial.println(mcp_state);
-      
       break;
 
-    case PIN_S_DOOR:
-      Serial.print("PIN_S_DOOR IO has changed to ");
+    case PIN_BRAKEPADS:
+      Serial.print("PIN_BRAKEPADS IO has changed to ");
       Serial.println(mcp_state);
-    
+      if(mcp_state == true){
+        tft.drawXBitmap(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], sym_brakepads, 50, 50, TFT_ORANGE);
+      }
+      else{
+        tft.fillRect(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], 50, 50, BACK_COLOR);
+      }
       break;
 
-    case PIN_TRUNK:
-      Serial.print("PIN_TRUNK IO has changed to ");
+    case PIN_MOTOR_CAP:
+      Serial.print("PIN_MOTOR_CAP IO has changed to ");
       Serial.println(mcp_state);
-    
+      // if(mcp_state == true){
+      //   tft.drawXBitmap(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], sym_brakepads, 50, 50, TFT_ORANGE);
+      // }
+      // else{
+      //   tft.fillRect(Icon_Pos_BrakePads[0], Icon_Pos_BrakePads[1], 50, 50, BACK_COLOR);
+      // }
       break;
+
+    case PIN_WASHER_FLUID:
+      Serial.print("PIN_WASHER_FLUID IO has changed to ");
+      Serial.println(mcp_state);
+      if(mcp_state == true){
+        tft.drawXBitmap(Icon_Pos_WasherFluid[0], Icon_Pos_WasherFluid[1], sym_washer_fluid, 50, 50, TFT_ORANGE);
+      }
+      else{
+        tft.fillRect(Icon_Pos_WasherFluid[0], Icon_Pos_WasherFluid[1], 50, 50, BACK_COLOR);
+      }
+      break;
+
+    case PIN_BRAKESYSTEM:
+      Serial.print("PIN_BRAKESYSTEM IO has changed to ");
+      Serial.println(mcp_state);
+      if(mcp_state == true){
+        tft.drawXBitmap(Icon_Pos_BrakeSystem[0], Icon_Pos_BrakeSystem[1], sym_brakesystem, 50, 50, TFT_RED);
+      }
+      else{
+        tft.fillRect(Icon_Pos_BrakeSystem[0], Icon_Pos_BrakeSystem[1], 50, 50, BACK_COLOR);
+      }
+      break;
+
+    // case PIN_OIL_LEVEL:
+    //   Serial.print("PIN_OIL_LEVEL IO has changed to ");
+    //   Serial.println(mcp_state);
+    //   if(mcp_state == true){
+    //     tft.drawXBitmap(Icon_Pos_Oil[0], Icon_Pos_Oil[1], sym_oil, 50, 50, TFT_ORANGE);
+    //   }
+    //   else{
+    //     tft.fillRect(Icon_Pos_Oil[0], Icon_Pos_Oil[1], 50, 50, BACK_COLOR);
+    //   }
+    //   break;
+
+
 
     default:break;
   }
